e6f7be69eb4d9f9ddc8fe7dd2a15afc086180326
djcol14 <declan.coleman42@gmail.com>
2026-09-13 18:08:20 -0500

    This is the first functional skeleton with product pages

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
-function openProduct(product) {
-    alert("You selected the " + product);
+function openProduct(product){
+    if(product === 'sword'){
+        window.location.href = 'sword.html';   
+    }else{
+        if(product === "chainmail"){
+            window.location.href = "chainmail.html";
+        }else{
+            if(product === "platearmor"){
+                window.location.href = "platearmor.html";
+            }else{
+                if(product === "cookingpot"){
+                    window.location.href = "cookingpot.html";
+                }
+            }
+        }
+    }
 }
+
+
+
+const products = {
+    sword: {
+        name: "Sword",
+        price: 75
+    },
+    chainmail: {
+        name: "Chainmail",
+        price: 100
+    },
+    platearmor: {
+        name: "Plate Armor",
+        price: 200
+    },
+    cookingpot: {
+        name: "Cooking Pot",
+        price: 25
+    }
+};
+let quantity = 1;
+
+const productID = document.body.dataset.product;
+
+const product = products[productID];
+
+function updatePrice(){
+    const totalPrice = product.price * quantity;
+    
+    document.getElementById("quantity").textContent = quantity;
+
+    document.getElementById("total-price").textContent = totalPrice;
+}
+function changeQuantity(amount){
+    quantity = quantity + amount;
+
+    if(quantity < 1){
+        quantity = 1;
+    }
+    updatePrice();
+}
+updatePrice();
