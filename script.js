42228904782830cf67c27cbc750770c1dad49290
djcol14 <declan.coleman42@gmail.com>
2026-09-20 12:33:53 -0500

    Cart quantity and remove functions created

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function navindex(){
 
 function addtocart(product){
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    let quantity = document.getElementById("quantity").textContent;
+    let quantity = Number(document.getElementById("quantity").textContent);
     let item = {
         product: product,
         quantity: quantity
@@ -80,19 +80,69 @@ function addtocart(product){
     cart.push(item);
     console.log(item);
     localStorage.setItem("cart", JSON.stringify(cart));
+    window.location.href = "cart.html";
 }
 
 function displaycart(){
     console.log("displaycart function is running");
     let cart = JSON.parse(localStorage.getItem("cart"));
     console.log(cart);
+    document.querySelector(".item-list").textContent = "";
     for (let i = 0; i < cart.length; i++) {
+        console.log("loop is running");
         let product = cart[i].product;
         let quantity = cart[i].quantity;
         console.log(product);
-        document.querySelector(".item-list").textContent += "\n" + product + " x " + quantity;
         let itemDiv = document.createElement("div");
+        console.log("itemDiv Created");
+        itemDiv.textContent = product + " x ";
+        console.log("itemDiv text added");
+        document.querySelector(".item-list").appendChild(itemDiv);
+        console.log("itemDiv appended");
+        let quantityDisplay = document.createElement("span");
+        quantityDisplay.textContent = quantity;
+        itemDiv.appendChild(quantityDisplay);
+        let subQuantitySelector = document.createElement("button");
+        subQuantitySelector.textContent = "-";
+        subQuantitySelector.addEventListener("click", function() {
+            decreaseCartQuantity(i, quantityDisplay);
+        });
+        itemDiv.appendChild(subQuantitySelector);
+        let addQuantitySelector = document.createElement("button");
+        addQuantitySelector.textContent = "+";
+        addQuantitySelector.addEventListener("click", function() {
+            increaseCartQuantity(i, quantityDisplay);
+        });
+        itemDiv.appendChild(addQuantitySelector);
+        let removeItemButton = document.createElement("button");
+        removeItemButton.textContent = "Remove";
+        removeItemButton.addEventListener("click", function() {
+            removeCartItem(i);
+        });
+        itemDiv.appendChild(removeItemButton);
     }
+}
+
+function decreaseCartQuantity(i, quantityDisplay){
+    console.log("DECREASE FUNCTION RAN");
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart[i].quantity--;
+    localStorage.setItem("cart", JSON.stringify(cart));
+    quantityDisplay.textContent = cart[i].quantity;
+}
+
+function increaseCartQuantity(i, quantityDisplay){
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart[i].quantity++;
+    localStorage.setItem("cart", JSON.stringify(cart));
+    quantityDisplay.textContent = cart[i].quantity;
+}
+
+function removeCartItem(i) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.splice(i,1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    displaycart();
 }
 if (document.querySelector(".item-list")) {
     displaycart();
